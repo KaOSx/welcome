d243ce432eace266c3ddd44f8e8be0fba0cc86a4
demmm <anke62@gmail.com>
2016-01-09 17:30:40 -0500

    update translations, id & hr

--- translations/translateFile_id_ID.ts
+++ translations/translateFile_id_ID.ts
@@ -14,7 +14,7 @@
     <message>
         <location filename="mainwindow.ui" line="186"/>
         <source>Passwords</source>
-        <translation>Kata Sandi</translation>
+        <translation>Kata-sandi</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="269"/>
@@ -49,7 +49,7 @@ p, li { white-space: pre-wrap; }
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Oxygen-Sans&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;Terimakasih telah mencoba KaOS. Memerlukan berkas kata sandi atau daftar paket?  Ingin mempelajari lebih lanjut tentang proyek?  Silakan cari tautan di bawah.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;Terimakasih telah mencoba KaOS. Memerlukan berkas kata-sandi atau daftar paket?  Ingin mempelajari lebih lanjut tentang proyek?  Silakan cari tautan di bawah.&lt;/span&gt;&lt;/p&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:12pt;&quot;&gt;Jangan ragu untuk memberikan opini Anda tentang KaOS di Forum.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
